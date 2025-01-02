5efea84b694890ba4d6db681bf199294fd18bd04
Masaaki Goshima <goccy54@gmail.com>
2025-01-02 11:03:51 +0900

    support wasip1

---
+++ fontdirs_others.go
@@ -0,0 +1,15 @@
+//go:build wasip1
+
+package findfont
+
+func getFontDirectories() (paths []string) {
+	return nil
+}
+
+func getUserFontDirs() (paths []string) {
+	return nil
+}
+
+func getSystemFontDirs() (paths []string) {
+	return nil
+}
